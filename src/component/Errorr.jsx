112f283cbbb2a01045f1e7d53a26d8bf11832de9
bmayuri20 <bmayuri100@gmail.com>
2025-09-20 18:03:25 +0530

    added div

--- src/component/Errorr.jsx
+++ src/component/Errorr.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 function Errorr() {
-  return (
+  return (<>
     <div>
         <h1 className='border border-danger text-red'>The page you Looking is not found</h1>
     </div>
+    <div>
+        {/* added div */}
+    </div>
+    </>
   )
 }
 
